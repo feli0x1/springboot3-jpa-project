1e83918d265b28b38662a37549190e2dc621f257
feli0x1 <sdiylaogzbhk@proton.me>
2023-08-30 11:13:59 -0300

    Project creation

---
+++ src/main/java/com/educandoweb/educationaldemo/EducationalDemoApplication.java
@@ -0,0 +1,11 @@
+package com.educandoweb.educationaldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationalDemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(EducationalDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/educandoweb/educationaldemo/EducationalDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educandoweb.educationaldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationalDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
