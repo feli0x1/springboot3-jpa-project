6511ec98b789309341bdb2b714f876597642592d
feli0x1 <sdiylaogzbhk@proton.me>
2023-08-31 13:56:04 -0300

    Service layer and component registration implemented

--- src/main/java/com/educandoweb/educationaldemo/resources/UserResource.java
+++ src/main/java/com/educandoweb/educationaldemo/resources/UserResource.java
@@ -1,19 +1,33 @@
 package com.educandoweb.educationaldemo.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.educandoweb.educationaldemo.entities.User;
+import com.educandoweb.educationaldemo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User user1 = new User(1L, "James", "james@gmail.com", "123456789", "password123");
-		return ResponseEntity.ok().body(user1);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(user);
 	}
 }
