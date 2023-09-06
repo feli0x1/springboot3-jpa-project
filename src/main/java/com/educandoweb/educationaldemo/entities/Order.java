9f4b08ac200731ac2d75f22f92565822ce40e3b8
feli0x1 <sdiylaogzbhk@proton.me>
2023-09-06 12:59:01 -0300

    Subtotal e total methods implemented

--- src/main/java/com/educandoweb/educationaldemo/entities/Order.java
+++ src/main/java/com/educandoweb/educationaldemo/entities/Order.java
@@ -102,6 +102,14 @@ public class Order implements Serializable {
 		return items;
 	}
 	
+	public Double getTotal() {
+		double total = 0.0;
+		for (OrderItem oi : items) {
+			total += oi.getSubTotal();
+		}
+		return total;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
--- src/main/java/com/educandoweb/educationaldemo/entities/Payment.java
+++ src/main/java/com/educandoweb/educationaldemo/entities/Payment.java
@@ -3,6 +3,8 @@ package com.educandoweb.educationaldemo.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -24,6 +26,7 @@ public class Payment implements Serializable {
 	
 	@OneToOne
 	@MapsId
+	@JsonIgnore
 	private Order order;
 	
 	public Payment() {
